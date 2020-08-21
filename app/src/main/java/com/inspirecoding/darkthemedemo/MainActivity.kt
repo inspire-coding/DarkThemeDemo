2aca8e34800b728a3a66fe913d3337f79e25d9ee
inspire-coding <+PaZ+PeTi+88113+>
2020-08-21 06:27:15 +0200

    Finished project

--- app/src/main/java/com/inspirecoding/darkthemedemo/MainActivity.kt
+++ app/src/main/java/com/inspirecoding/darkthemedemo/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.inspirecoding.darkthemedemo
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
 
 class MainActivity : AppCompatActivity()
 {
@@ -9,5 +12,22 @@ class MainActivity : AppCompatActivity()
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 1
+        val switch = findViewById<SwitchCompat>(R.id.switch_darkLight)
+
+        // 2
+        switch.setOnClickListener {
+            // 3
+            val isNightTheme = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+            // 4
+            when (isNightTheme)
+            {
+                Configuration.UI_MODE_NIGHT_YES ->
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                Configuration.UI_MODE_NIGHT_NO ->
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
+        }
     }
 }
